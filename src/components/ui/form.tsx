3f6e01352e108c548086c84a345165e13532f05d
tricrck <tricrck@gmail.com>
2026-01-25 15:12:41 +0300

    build fix 1

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
-import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from "react-hook-form";
-
+import { type ControllerProps, type FieldPath, type FieldValues, Controller, FormProvider, useFormContext } from 'react-hook-form'
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 
