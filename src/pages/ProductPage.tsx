7af7cb00957b99f0d5b339ee2b3e59f9e574bfef
tricrck <tricrck@gmail.com>
2026-02-27 13:07:17 +0300

    remove cta in product page.

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,13 +1,13 @@
 import { useParams, Navigate } from "react-router-dom";
 import { getProductById } from "@/components/data/products";
 import Header from "@/components/Header";
-// import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/sections/FeaturesSection";
 import ProductKitSection from "@/components/sections/ProductKitSection";
 import UseCasesSection from "@/components/sections/UseCasesSection";
 import WhyCustomersLoveSection from "@/components/sections/WhyCustomersLoveSection";
 import GallerySection from "@/components/sections/GallerySection";
-import CTASection from "@/components/sections/CTASection";
+// import CTASection from "@/components/sections/CTASection";
 import OrderFormSection from "@/components/sections/OrderFormSection";
 import FooterSection from "@/components/sections/FooterSection";
 import { useEffect } from "react";
@@ -28,12 +28,13 @@ const ProductPage = () => {
   return (
     <main>
       <Header />
+      <HeroSection product={product}/>
       <FeaturesSection product={product} />
       {hasKit     && <ProductKitSection product={product} />}
       {hasUses    && <UseCasesSection   product={product} />}
       {hasGallery && <GallerySection    product={product} />}
       <WhyCustomersLoveSection product={product} />
-      <CTASection              product={product} />
+      {/* <CTASection              product={product} /> */}
       <OrderFormSection        product={product} />
       <FooterSection />
     </main>
